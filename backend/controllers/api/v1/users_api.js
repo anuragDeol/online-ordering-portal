bf4e05a4b36481146e1879818dee2383101d4f4b
Anurag Deol <anuragdeol2017@gmail.com>
2023-01-28 18:42:20 +0530

    user-login

--- backend/controllers/api/v1/users_api.js
+++ backend/controllers/api/v1/users_api.js
@@ -42,3 +42,37 @@ module.exports.register = async function (req, res) {
     });
   }
 };
+
+module.exports.login = async function (req, res) {
+  try {
+    let user = await User.findOne({ 
+        phone_number: req.body.phone_number 
+    });
+    if (!user) {
+      return res.json(404, {
+        message: "Failure! User not found. Please register first.",
+      });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.json(401, {
+        message: "Failure! Incorrect credentials.",
+      });
+    }
+
+    const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, { expiresIn: "3600000"});
+    user = user.toObject();
+    return res.json(200, {
+      message: `Success! ${user.name}, you are logged in.`,
+      data: {
+        user: user,
+        token: token,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json(500, {
+      message: "Internal Server Error",
+    });
+  }
+};
--- backend/routes/api/v1/users.js
+++ backend/routes/api/v1/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const usersController = require("../../../controllers/api/v1/users_api");
 
 router.post("/add-user", usersController.register); // user registration - add a new user
-// router.post("/login", usersController.login); // user login
+router.post("/login-user", usersController.login); // user login
 
 module.exports = router;
